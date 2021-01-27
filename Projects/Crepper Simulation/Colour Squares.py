92a709e47aa04527bd6d48fa0a9b9d4a754903b6
DeceitfulSloth <57531397+DeceitfulSloth@users.noreply.github.com>
2021-01-27 11:04:32 +0000

    Update Colour Squares.py
    heading for more parameterisation

--- Projects/Crepper Simulation/Colour Squares.py
+++ Projects/Crepper Simulation/Colour Squares.py
@@ -7,9 +7,11 @@ from pygame.locals import *
 pygame.init()
 
 #Create game window
-windowSurface = pygame.display.set_mode((600,600), 0, 32)
-pygame.display.set_caption('Hello world!')
+WIDTH = 1800
+HEIGHT = 900
 
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption('Colour squares')
 
 
 # Set up colours
@@ -30,8 +32,9 @@ pixArray = pygame.PixelArray(windowSurface)
 
 # Set parameters
 offset = (100,100)
-size = (400, 400)
-totalSections = (7,7)
+size = (WIDTH - 200, HEIGHT - 200)
+
+totalSections = (size[0]/100, size[1]/100)
 
 for x in range(offset[0],offset[0]+size[0]):
     for y in range(offset[1],offset[1]+size[1]):
@@ -49,7 +52,8 @@ for x in range(offset[0],offset[0]+size[0]):
         yexpanded = int((ydiscretized * 255) / totalSections[1])
         
         
-        pixArray[x][y] = (0, yexpanded, xexpanded)
+
+        pixArray[x][y] = (xexpanded,yexpanded,0)
 
 del pixArray
 
@@ -62,3 +66,5 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    
