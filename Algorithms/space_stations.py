0d5800d7e1d4c0706d53543f65ed92cc648ba945
DeceitfulSloth <57531397+DeceitfulSloth@users.noreply.github.com>
2021-09-13 13:20:10 +0000

    Added click to add or remove space stations

--- Algorithms/space_stations.py
+++ Algorithms/space_stations.py
@@ -5,6 +5,7 @@ import math
 
 #Create an instance of tkinter frame
 win= Tk()
+win.resizable(width=False, height=False)
 
 # Define constants
 WIDTH = 1200
@@ -18,7 +19,7 @@ RIGHT_MARGIN = 10
 win.geometry(str(WIDTH) + "x" + str(HEIGHT))
 
 # Fix random state
-random.seed(42)
+#random.seed(42)
 
 #Create a canvas object
 c= Canvas(win,width=WIDTH, height=HEIGHT, bg="black")
@@ -68,7 +69,7 @@ def is_within(a, b, r):
 
 # Generate a list of 50 stations
 stations = []
-for i in range(100):
+for i in range(75):
     stations.append(Station(random.randint(LEFT_MARGIN, WIDTH - RIGHT_MARGIN), random.randint(TOP_MARGIN, HEIGHT - BOTTOM_MARGIN)))
 
 print(f'There are {len(stations)} stations.')
@@ -76,7 +77,7 @@ print(f'There are {len(stations)} stations.')
 # Mark overlapping stations
 for i in stations:
     for j in stations:
-        if i != j and i.distance(j) < 50:
+        if i != j and i.distance(j) < 75:
             stations.remove(j)
             
 
@@ -90,69 +91,107 @@ for i in range(len(stations)):
         if stations[i].distance(stations[j]) < 200:
             connections.append((stations[i],stations[j]))
 
-newConnections = []
-# Generate new station connections
-for i in range(len(stations)):
-    for j in range(i + 1, len(stations)):
-        if stations[i].distance(stations[j]) < 200:
-            newConnections.append((stations[i], stations[j], stations[i].distance(stations[j]), True))
 
-# Crop too long connections
-croppedConnections = []
+def generate_connections():
+    newConnections = []
+    # Generate new station connections
+    for i in range(len(stations)):
+        for j in range(i + 1, len(stations)):
+            if stations[i].distance(stations[j]) < 200:
+                newConnections.append((stations[i], stations[j], stations[i].distance(stations[j]), True))
 
-for i in stations:
-    # Smallest connections in 4 directions
-    LD_Min = float('inf')
-    LU_Min = float('inf')
-    RD_Min = float('inf')
-    RU_Min = float('inf')
-    LD_Con = None
-    LU_Con = None
-    RD_Con = None
-    RU_Con = None
+    # Crop too long connections
+    croppedConnections = []
 
-
-    for j in newConnections:
-        if i == j[0]: # If the station under consideration is the start of the connection
-            
-            
-            if j[0].get_x() < j[1].get_x() and j[0].get_y() < j[1].get_y():
-                if j[2] < RD_Min:
-                    RD_Con = j
+    for i in stations:
+        # Smallest connections in 4 directions
+        LD_Min = float('inf')
+        LU_Min = float('inf')
+        RD_Min = float('inf')
+        RU_Min = float('inf')
+        LD_Con = None
+        LU_Con = None
+        RD_Con = None
+        RU_Con = None
 
 
-            elif j[0].get_x() >= j[1].get_x() and j[0].get_y() < j[1].get_y():                
-                if j[2] < LD_Min:
-                    LD_Con = j
-
-            if j[0].get_x() < j[1].get_x() and j[0].get_y() >= j[1].get_y():                
-                if j[2] < RU_Min:
-                    RU_Con = j
-
-            else:
-                if j[2] < LU_Min:
-                    LU_Con = j
-
-    croppedConnections.extend([LD_Con, LU_Con, RD_Con, RU_Con])        
-    #print(f"LD_Con: {LD_Con}")
-    #print(f"LU_Con: {LU_Con}")
-    #print(f"RD_Con: {RD_Con}")
-    #print(f"RU_Con: {RU_Con}")
-    #print()
+        for j in newConnections:
+            if i == j[0]: # If the station under consideration is the start of the connection
 
 
+                if j[0].get_x() < j[1].get_x() and j[0].get_y() < j[1].get_y():
+                    if j[2] < RD_Min:
+                        RD_Con = j
 
 
-#for i in connections:
-    #draw_line(c, i[0].get_x(), i[0].get_y(), i[1].get_x(), i[1].get_y(), "cyan")
+                elif j[0].get_x() >= j[1].get_x() and j[0].get_y() < j[1].get_y():                
+                    if j[2] < LD_Min:
+                        LD_Con = j
 
-for i in croppedConnections:
-    if i != None:
-        draw_line(c, i[0].get_x(), i[0].get_y(), i[1].get_x(), i[1].get_y(), "cyan")
+                if j[0].get_x() < j[1].get_x() and j[0].get_y() >= j[1].get_y():                
+                    if j[2] < RU_Min:
+                        RU_Con = j
 
-# Display stations
-for i in stations:
-    draw_circle(c,i.get_x(), i.get_y(), 8, i.get_color())
+                else:
+                    if j[2] < LU_Min:
+                        LU_Con = j
 
+        croppedConnections.extend([LD_Con, LU_Con, RD_Con, RU_Con])        
+    return croppedConnections
+
+croppedConnections = generate_connections()
+
+
+
+def render(con, stat):
+    # Clear screen
+    c.delete("all")
+
+    for i in con:
+        if i != None:
+            draw_line(c, i[0].get_x(), i[0].get_y(), i[1].get_x(), i[1].get_y(), "cyan")
+
+    # Display stations
+    for i in stat:
+        draw_circle(c,i.get_x(), i.get_y(), 8, i.get_color())
+
+render(croppedConnections, stations)
+
+
+def click_handler(event):
+    # event also has x & y attributes
+    # Add a station
+    if event.num == 1:
+        stations.append(Station(event.x, event.y))
+        croppedConnections = generate_connections()
+        render(croppedConnections, stations)
+
+    # Delete a station
+    elif event.num == 3:
+        #print("x,y", event.x, event.y)
+        closestStation = None
+        minDistance = float("inf")
+        for i in stations:
+            #print("station x,y", i.get_x(), i.get_y(), math.sqrt(pow(i.get_x() - event.x, 2) + pow(i.get_y() - event.y, 2)))
+            if math.sqrt(pow(i.get_x() - event.x, 2) + pow(i.get_y() - event.y, 2)) < minDistance:
+                closestStation = i
+                minDistance = math.sqrt(pow(i.get_x() - event.x, 2) + pow(i.get_y() - event.y, 2))
+        
+        stations.remove(closestStation)
+        #print("closest station x,y", closestStation.get_x(), closestStation.get_y())
+        croppedConnections = generate_connections()
+        render(croppedConnections, stations)
+
+
+def on_mousewheel(event):
+    #self.canvas.yview_scroll(-1*(event.delta/120), "units")
+    print(event)
+
+
+c.bind_all("<MouseWheel>", on_mousewheel)
+
+
+
+
+win.bind("<Button>", click_handler)
 win.mainloop()
-
